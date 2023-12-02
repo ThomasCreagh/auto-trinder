2c15c76aadcd6be400dbc9f8db5da9fcf58f8adc
untab <tom99ato@gmail.com>
2023-12-02 18:28:54 +0000

    made the google sheet integration

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
 from lib import TrinderImage
+import pygsheets
+from datetime import datetime
 
-test_image = TrinderImage('''TEST !! 1234567890 Hello, world! 👋 Ive desiced to type more words with my fingers. OKAY THIS IS ENOUGH IALL I WANT IS TO TEST MY PROGRAM WITH AN EXTREMETLY LARGE TEST SO IT DECIDES TO CHOOSE THE CORRECT SIZE OF FONT, AND WORD WRAP TO HAVE IT GET THE MAX BOX SIZE!! If my fingers keep typing then they might get sore. I actually cant decide what to do about this mess of text; lol thats a semi colon! OK OK I NEED TO STOP. I HAVE AN ASSIGNMENT DUE IN LIKE 1 HOUR AND 57 MINUTES!!!!!!!!!! THATS CRAZY Here are some emojis: 🎨 🌊 😎''',
-                          "data/background.jpeg",
-                          "data/Arial.ttf",
-                          "output/image.png")
+client = pygsheets.authorize(service_account_file="auto-trinder-e79abbfcea61.json") 
+spreadsheet = client.open(client.spreadsheet_titles()[0]) 
+
+wks = spreadsheet.sheet1
+title = True
+for row in wks:
+    if title: 
+        title = False
+        continue
+    
+    TrinderImage(row[2],
+                  "data/background.jpeg",
+                  "data/Arial.ttf",
+                  f"output/{datetime.now()}.png")
+
+
+# print(client.spreadsheet_titles()) 
+
+# test_image = TrinderImage('''Got the assignmet done with 3 mins to spare 😎''',
+#                           "data/background.jpeg",
+#                           "data/Arial.ttf",
+#                           "output/image.png")
+
+
